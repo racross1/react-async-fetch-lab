a2378aed0f9c6dc845c057bcbbd3c088a4826a13
racross1 <racross1@gmail.com>
2021-02-05 14:36:11 -0800

    commit tree 01a6b3875c4f8b0bfc1dc7b581fb607ac6910f74

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { Component } from 'react';
+
+// import Timer from './Timer'
+// import Controls from './Controls'
+
+class App extends Component {
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json)
+        .then(console.log)
+    }
+
+    render(){
+        return(
+            <div>Hi!</div>
+        )
+
+    }
+}
+
+export default App
